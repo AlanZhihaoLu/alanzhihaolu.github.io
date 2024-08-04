2f94b05648d1d81afddbc04af8ed86ddd76de637
AlanZhihaoLu <alanzhihaolu@gmail.com>
2024-08-03 20:24:32 -0400

    added demos

---
+++ demos/inverted-scene-gist/src/input/additional_stimuli.js
@@ -0,0 +1,94 @@
+attention_checks = [
+  {
+    "SceneImage": "red.jpg",
+    "probe": "Swimming Pool",
+    "consistency_category": "AttentionCheck",
+    "Consistent": "AttentionCheck",
+    "Inverted": "No",
+    "Duration": 250
+  },
+  {
+    "SceneImage": "orange.jpg",
+    "probe": "Golf Course",
+    "consistency_category": "AttentionCheck",
+    "Consistent": "AttentionCheck",
+    "Inverted": "No",
+    "Duration": 250
+  },
+  {
+    "SceneImage": "green.jpg",
+    "probe": "Skyscraper",
+    "consistency_category": "AttentionCheck",
+    "Consistent": "AttentionCheck",
+    "Inverted": "No",
+    "Duration": 250
+  },
+  {
+    "SceneImage": "blue.jpg",
+    "probe": "Mountain",
+    "consistency_category": "AttentionCheck",
+    "Consistent": "AttentionCheck",
+    "Inverted": "No",
+    "Duration": 250
+  },
+  {
+    "SceneImage": "gray.jpg",
+    "probe": "Pineapple",
+    "consistency_category": "AttentionCheck",
+    "Consistent": "AttentionCheck",
+    "Inverted": "No",
+    "Duration": 250
+  },
+  {
+    "SceneImage": "red.jpg",
+    "probe": "Banana",
+    "consistency_category": "AttentionCheck",
+    "Consistent": "AttentionCheck",
+    "Inverted": "No",
+    "Duration": 250
+  },
+  {
+    "SceneImage": "orange.jpg",
+    "probe": "Airplane",
+    "consistency_category": "AttentionCheck",
+    "Consistent": "AttentionCheck",
+    "Inverted": "No",
+    "Duration": 250
+  },
+  {
+    "SceneImage": "green.jpg",
+    "probe": "Fire Truck",
+    "consistency_category": "AttentionCheck",
+    "Consistent": "AttentionCheck",
+    "Inverted": "No",
+    "Duration": 250
+  },
+  {
+    "SceneImage": "blue.jpg",
+    "probe": "Apple",
+    "consistency_category": "AttentionCheck",
+    "Consistent": "AttentionCheck",
+    "Inverted": "No",
+    "Duration": 250
+  },
+  {
+    "SceneImage": "gray.jpg",
+    "probe": "Guitar",
+    "consistency_category": "AttentionCheck",
+    "Consistent": "AttentionCheck",
+    "Inverted": "No",
+    "Duration": 250
+  }
+]
+
+practice_stimuli = [
+    {
+      "SceneImage": "coffeemaker.jpg",
+      "probe": "Microwave",
+      "consistency_category": "Practice",
+      "Consistent": "Practice",
+      "Inverted": "No",
+      "Duration": 100,
+      "practice": "Yes"
+    }
+  ]
